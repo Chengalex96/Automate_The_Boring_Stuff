6056e366c312431aaca632f09029cc625cf81fad
Chengalex96 <chengalex96@gmail.com>
2024-07-25 11:05:36 -0400

    Update listmethods

--- listMethods.py
+++ listMethods.py
@@ -1,3 +1,10 @@
 spam = ['cat', 'dog', 'cat']
+print(spam.index('cat'))
 spam.remove('cat')
 print(spam)
+
+# index, append to the end, insert(1, 'chicken), remove (first instance)
+# Lists are modified in-place
+# spam.sort() - ints or strings by alphabetical, can use reverse=True
+# Cannot mix ints and strings
+# Sorts with ASCII-betical order, pass key = str.lower
