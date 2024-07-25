92b52624c5e1ed49f28948dd6cfb3608745a9979
Chengalex96 <chengalex96@gmail.com>
2024-07-25 10:57:59 -0400

    Update listrange

--- ListRange.py
+++ ListRange.py
@@ -14,3 +14,12 @@ spam = 42
 spam += 1
 print(spam)
 
+print(cat[1:]+cat[:-1])
+
+print(cat*2)
+
+# Indexes start at 0, not 1
+# Negative index to refer to last items
+# Slice (:), goes up to but not including the last item
+# len() works the same as lists with multiplication, and concatenation
+# Can covert a value into a list using list()
