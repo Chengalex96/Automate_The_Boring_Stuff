4780cec323380a6cab137eab0c2b5e3b5b622520
Chengalex96 <chengalex96@gmail.com>
2024-07-30 14:49:45 -0400

    RelativePaths

--- Filesnames.py
+++ Filesnames.py
@@ -1,3 +1,32 @@
 # Files seperator
 
 # Windows is \
+
+import os
+os.path.join('folder1', 'folder2', 'folder3')
+
+print(os.sep) # Returns \ on windows
+
+print(os.getcwd())
+
+# Absolute file path (start with root folder) and relative file path
+# . refers to this folder
+
+print(os.path.abspath('MVO Constraints'))
+
+print(os.path.relpath('MVO Constraints', 'c:\\Users\alex_'))
+
+print(os.listdir(r'C:\Users\alex_\.vscode\Python\Automate the Boring Stuff\Automate_The_Boring_Stuff'))
+
+print(os.path.getsize(r'C:\Users\alex_\.vscode\Python\Automate the Boring Stuff\Automate_The_Boring_Stuff\dict.py'))
+
+# Find the total size of a folder
+
+totalSize = 0
+
+for filename in  os.listdir(r'C:\Users\alex_\.vscode\Python\Automate the Boring Stuff\Automate_The_Boring_Stuff'):
+    if not os.path.isfile(os.path.join(r'C:\Users\alex_\.vscode\Python\Automate the Boring Stuff\Automate_The_Boring_Stuff', filename)):
+        continue
+    totalSize = totalSize + os.path.getsize(os.path.join(r'C:\Users\alex_\.vscode\Python\Automate the Boring Stuff\Automate_The_Boring_Stuff', filename))
+
+print(totalSize)
